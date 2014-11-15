d88d0515e69fdedb6b4f7c013bf48806c772d206
Darmy <Darmy@147.32.118.126>
2014-11-15 22:18:41 +0100

    Leaderboards saving and loading done

--- src/pingpong/LeaderboardsPanel.java
+++ src/pingpong/LeaderboardsPanel.java
@@ -31,7 +31,7 @@ public class LeaderboardsPanel extends JPanel {
 
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private  LeaderboardsDrawPanel drawPanel;
+    private LeaderboardsDrawPanel drawPanel;
 
     private DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
     private Date date;
@@ -46,56 +46,32 @@ public class LeaderboardsPanel extends JPanel {
     public LeaderboardsPanel() {
         init();
 
-        /*names = new File("names.txt");
-         scores = new File("scores.txt");
-         dates = new File("dates.txt");
-
-         try {
-         updateLeaderboards(names, 0);
-         updateLeaderboards(scores, 1);
-         updateLeaderboards(dates, 2);
-         } catch (Exception ex) {
-         /*Nic nevyskakuje, abychom si nevystrasili uzivatele!*/
-        /*JOptionPane.showMessageDialog(this, "Saves not found.\nCreating files...");*/
-        /*} finally {
-         updateFiles(names, 0);
-         updateFiles(scores, 1);
-         updateFiles(dates, 2);
-         }*/
     }
 
     private void init() {
         statsFile = new File("stats.txt");
         date = new Date();
-        
-        for (int i = 0; i < leaderboard.length; i++) {
-            leaderboard[i][0] = "Player";
-            leaderboard[i][1] = "0";
-            leaderboard[i][2] = dateFormat.format(date);
-        }
-        
-        drawPanel = new LeaderboardsDrawPanel(leaderboard, 100,0);
-        //drawPanel.setLocation(100, 0);
+
+        initLeaderBoards();
+
+        drawPanel = new LeaderboardsDrawPanel(leaderboard, 100, 0);
         initLabel1();
         initLabel2();
         initLayout();
 
-        saveStats();
-
-        //repaint();
-
-        /*try {
-         loadStats();
-         } catch (IOException ex) {
-         //IO ex while reading line of stats file
-         }*/
+        //saveStats();
+        try {
+            loadStats();
+        } catch (IOException ex) {
+            JOptionPane.showMessageDialog(this, "Error when reading the statsFile!", "Error", JOptionPane.ERROR_MESSAGE);
+        }
     }
 
-    
     private void loadStats() throws IOException {
         String line;
-        String[] words;
         boolean fileFound = true;
+        int nameScoreDate = 0; ///< Names = 0, Scores = 1, Dates = 2
+        int j = 0; ///< Line in leaderboard
 
         try {
             bfr = new BufferedReader(new FileReader(statsFile));
@@ -105,30 +81,34 @@ public class LeaderboardsPanel extends JPanel {
         }
 
         if (fileFound) {
-            for (int i = 0; i < leaderboard.length; i++) {
+            for (int i = 0; i < (leaderboard.length * leaderboard[0].length); i++) {
+                j = i % 10;
+                if (i == leaderboard.length || i == leaderboard.length * 2) {
+                    nameScoreDate++;
+                }
                 line = bfr.readLine();
-                words = line.split(" ");
-                leaderboard[i] = words;
+                leaderboard[j][nameScoreDate] = line;
             }
 
             bfr.close();
         }
 
     }
-    
+
     /**
-     * Saves Leaderboard to the statsFile.
-     * Firstly writes all the names then all the scores and then all the dates,
-     * every item at new line.
+     * Saves Leaderboard to the statsFile. Firstly writes all the names then all
+     * the scores and then all the dates, every item at new line.
      */
-    private void saveStats(){
+    private void saveStats() {
         int nameScoreDate = 0; ///< Names = 0, Scores = 1, Dates = 2
         int j; ///< Line in leaderboard
         try {
             bfw = new BufferedWriter(new FileWriter(statsFile));
             for (int i = 0; i < (leaderboard.length * leaderboard[0].length); i++) {
-                j = i%10;
-                if(i==leaderboard.length || i==leaderboard.length*2) nameScoreDate++;
+                j = i % 10;
+                if (i == leaderboard.length || i == leaderboard.length * 2) {
+                    nameScoreDate++;
+                }
                 bfw.write(leaderboard[j][nameScoreDate]);
                 bfw.newLine();
             }
@@ -137,20 +117,12 @@ public class LeaderboardsPanel extends JPanel {
             JOptionPane.showMessageDialog(this, "File not found!", "Error", JOptionPane.ERROR_MESSAGE);
         }
     }
-
-
-    /*Nacte data z leaderboard[][] pole a zapise je do souboru
-     nameScoreDate - name=0, score=1, date=2*/
-    private void updateFiles(File file, int nameScoreDate) {
-        try {
-            bfw = new BufferedWriter(new FileWriter(file));
-            for (int i = 0; i < leaderboard.length; i++) {
-                bfw.write(leaderboard[i][nameScoreDate]);
-                bfw.newLine();
-            }
-            bfw.close();
-        } catch (IOException ex) {
-            JOptionPane.showMessageDialog(this, "File not found!", "Error", JOptionPane.ERROR_MESSAGE);
+    
+    private void initLeaderBoards(){
+        for (int i = 0; i < leaderboard.length; i++) {
+            leaderboard[i][0] = "Player";
+            leaderboard[i][1] = "0";
+            leaderboard[i][2] = dateFormat.format(date);
         }
     }
 
@@ -199,5 +171,4 @@ public class LeaderboardsPanel extends JPanel {
         });
     }
 
-    
 }
